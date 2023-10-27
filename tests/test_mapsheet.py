5e514aa6a1ff0edb5c40fe4ea34d2846d8de0e1a
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-10-28 01:23:41 +0200

    Update tests

--- tests/test_mapsheet.py
+++ tests/test_mapsheet.py
@@ -1,7 +1,11 @@
+import json
+
 import numpy as np
 import pytest
 from src import Resolution, MapSheet
 from unittest.mock import patch
+
+from src.custom_types import PixelCoordinate
 
 
 # Test Resolution class
@@ -31,19 +35,30 @@ def test_percentage_size_type_error():
         Resolution.percentage_size("50")
 
 
-# Test MapSheet class
-def test_init():
-    sheet = MapSheet("Title", "Endpoint")
-    assert sheet.title == "Title"
+@patch("requests.get")
+def test_init(mock_get):
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.content = json.dumps({"width": 300, "height": 300})
+    sheet = MapSheet("Endpoint")
     assert sheet._image_endpoint == "Endpoint"
 
+def test_mapsheet_from_annotationpage(sample_annotationpage):
+    sheet = MapSheet.from_annotationpage(sample_annotationpage)
+    assert sheet.id == "7044e91ab1c516f6"
+    assert sheet._image_endpoint == "https://images.memorix.nl/nai/iiif/8a35a495-f3b3-d957-6404-fc7c870e998c"
+    assert sheet._mask.bottom_left == PixelCoordinate(796, 731)
+
+@pytest.mark.skip  # TODO, make this pass
+def test_mapsheet_to_annotationpage(sample_annotationpage):
+    sheet = MapSheet.from_annotationpage(sample_annotationpage)
+    assert sheet.to_annotationpage() == sample_annotationpage
 
 # Mocking request for get_image
 @patch("requests.get")
 def test_get_image(mock_get, sample_image_bytes):
     mock_get.return_value.status_code = 200
     mock_get.return_value.content = sample_image_bytes
-    sheet = MapSheet("Title", "Endpoint")
+    sheet = MapSheet("Endpoint")
     assert isinstance(sheet.get_image(), np.ndarray)  # Your own assertion here
 
 
@@ -52,7 +67,7 @@ def test_get_image(mock_get, sample_image_bytes):
 def test_get_image_region(mock_get, sample_image_bytes):
     mock_get.return_value.status_code = 200
     mock_get.return_value.content = sample_image_bytes
-    sheet = MapSheet("Title", "Endpoint")
+    sheet = MapSheet("Endpoint")
     assert isinstance(
         sheet.get_image_region(0, 0, 100, 100), np.ndarray
     )  # Your own assertion here
@@ -62,6 +77,6 @@ def test_get_image_region(mock_get, sample_image_bytes):
 @patch("requests.get")
 def test_get_image_failure(mock_get):
     mock_get.return_value.status_code = 404
-    sheet = MapSheet("Title", "Endpoint")
+    sheet = MapSheet("Endpoint")
     with pytest.raises(RuntimeError):
         sheet.get_image()
