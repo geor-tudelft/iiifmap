0a63ac8c2201aa7ea09dc74f9a7b21ae930f5413
Oliver J. Post <65900202+OliverJPost@users.noreply.github.com>
2023-10-28 01:19:38 +0200

    Prevent circular import

--- src/georeference/referencemap.py
+++ src/georeference/referencemap.py
@@ -1,5 +1,8 @@
-from .. import MapSheet
-from ..custom_types import Wgs84Coordinate, Cv2Image
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .. import MapSheet
+    from ..custom_types import Wgs84Coordinate, Cv2Image
 
 
 class SearchBoxGenerator:
@@ -10,11 +13,11 @@ class SearchBoxGenerator:
 
 
 class SearchBox:
-    top_left = Wgs84Coordinate
-    bottom_right = Wgs84Coordinate
+    top_left = "Wgs84Coordinate"
+    bottom_right = "Wgs84Coordinate"
 
 
 class ReferenceMap:
     _wms_link: str # Or should it be IIIF presentation?
 
-    def extract_searchbox(self, searchbox: "SearchBox") -> Cv2Image: ...
+    def extract_searchbox(self, searchbox: "SearchBox") -> "Cv2Image": ...
